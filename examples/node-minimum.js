8e58391626fc03214098d640e0921772e797c1f2
Tierney Cyren <hello@bnb.im>
2020-01-30 13:26:17 -0500

    fix: make standard happy

--- examples/node-minimum.js
+++ examples/node-minimum.js
@@ -6,7 +6,7 @@ async function getNodeMinimumEngine () {
   const engines = await mingine() // mingine returns a promise
   const outputPath = path.join(__dirname, '/node-minimum-output.json')
 
-  fs.writeFile(outputPath, engines ? JSON.stringify(engines, null, 2) : undefined, function(error) {  // will write the minimum usable version of node.js if there were any `engines` that included `node`, otherwise it'll return undefined
+  fs.writeFile(outputPath, engines ? JSON.stringify(engines, null, 2) : undefined, function (error) { // will write the minimum usable version of node.js if there were any `engines` that included `node`, otherwise it'll return undefined
     if (error) throw error
 
     console.log(`wrote output to ${outputPath}!`)
